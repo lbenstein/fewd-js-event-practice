6347f45591e274adcd9a8b8b6e2b10b439a2d5a2
Lara Benstein <lbenstein@Laras-Mac-Pro.local>
2019-07-24 13:35:55 -0400

    Finished practice file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 (function () {
+  var test = document.querySelectorAll("button");
+  var button1 = test[0];
+  var button2 = test[1];
+  var button3 = test[2];
 
-  // your js goes here
+  document.addEventListener('click', function(event) {
+    if (event.target === button1) {
+      button1.classList.toggle("button-danger"); }
+	else if (event.target === button2) {
+    button2.innerHTML = "you clicked me"; }
+	else if (event.target === button3) {
+    button2.innerHTML = "button two"; }
+	}, false);
 
 })();
